84201dad82a1aff5f9a08b4693d50c0883f4ff21
Sandip Thapa <sandipthapa386@gmail.com>
2023-08-30 11:21:58 +0545

    Grouping tests

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -3,14 +3,19 @@ import { Greet } from "./greet";
 
 // Greet should render the text hello and if a name is passed into the component
 // it should render hello followed by name
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
 
-test("Greet renders with name", () => {
-  render(<Greet name="Sandip" />);
-  const textElement = screen.getByText("Hello Sandip");
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  describe("Nested", () => {
+    test("renders with name", () => {
+      render(<Greet name="Sandip" />);
+      const textElement = screen.getByText("Hello Sandip");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
 });
