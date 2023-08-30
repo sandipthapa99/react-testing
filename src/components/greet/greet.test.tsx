07f4680249a521491867ca313a387ce068ab0224
Sandip Thapa <sandipthapa386@gmail.com>
2023-08-30 11:04:24 +0545

    Greet test using TDD approach

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
+// Greet should render the text hello and if a name is passed into the component
+// it should render hello followed by name
 test("Greet renders correctly", () => {
   render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
+  const textElement = screen.getByText("Hello");
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Greet renders with name", () => {
+  render(<Greet name="Sandip" />);
+  const textElement = screen.getByText("Hello Sandip");
   expect(textElement).toBeInTheDocument();
 });
