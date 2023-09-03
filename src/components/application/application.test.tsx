5a7d543d3c3802a3a6e353884a3f9b85de020877
Sandip Thapa <sandipthapa386@gmail.com>
2023-09-03 14:11:51 +0545

    getByRole options

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -4,8 +4,27 @@ import { Application } from "./application";
 describe("application", () => {
   test("renders correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox");
+
+    const pageHeading = screen.getByRole("heading", {
+      // name: "Job application form",
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      name: "Section 1",
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
     expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -1,31 +1,40 @@
 export const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
+    <>
+      <h1>Job application form</h1>
+      <h2>Section 1</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+        </div>
 
-      <div>
-        <label htmlFor="job-location">Job Location</label>
-        <select id="job-location">
-          <option value="">Select a country</option>
-          <option value="US">United State</option>
-          <option value="UK">United Kingdom</option>
-          <option value="CA">Canada</option>
-          <option value="IN">India</option>
-          <option value="AU">AUstralia</option>
-        </select>
-      </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio" name="bio" />
+        </div>
 
-      <div>
-        <label>
-          <input type="checkbox" id="terms" />I agrre to the terms and
-          conditions
-        </label>
-      </div>
+        <div>
+          <label htmlFor="job-location">Job Location</label>
+          <select id="job-location">
+            <option value="">Select a country</option>
+            <option value="US">United State</option>
+            <option value="UK">United Kingdom</option>
+            <option value="CA">Canada</option>
+            <option value="IN">India</option>
+            <option value="AU">AUstralia</option>
+          </select>
+        </div>
 
-      <button>Submit</button>
-    </form>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" />I agrre to the terms and
+            conditions
+          </label>
+        </div>
+
+        <button>Submit</button>
+      </form>
+    </>
   );
 };
